de378e047be52a6262060da68cf43709d3e427f0
Gokul Das M <Gokul@NTP-LAP-723.chn.nestgroup.net>
2022-11-25 18:51:13 +0530

    viewfriend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddFriendComponent } from './add-friend/add-friend.component';
 import { SearchFriendComponent } from './search-friend/search-friend.component';
+import { ViewFriendComponent } from './view-friend/view-friend.component';
 
 const routes: Routes = [
   {path:"",component:AddFriendComponent},
-  {path:"search",component:SearchFriendComponent}
+  {path:"search",component:SearchFriendComponent},
+  {path:"view",component:ViewFriendComponent}
 ];
 
 @NgModule({
